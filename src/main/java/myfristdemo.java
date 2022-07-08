6ca80826b5aeeeabfda52bf60546dea0c243c230
BigdataShuai <108908446+BigdataShuai@users.noreply.github.com>
2022-07-08 14:43:27 +0800

    Update myfristdemo.java

--- src/main/java/myfristdemo.java
+++ src/main/java/myfristdemo.java
@@ -2,5 +2,6 @@ public class myfristdemo {
     public static void main(String[] args) {
         System.out.println("hello first");
         System.out.println("hello second");
+        System.out.println("hello third");
     }
 }
